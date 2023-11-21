c292da8b9bbd42e60af61955e6f079265d8f20f0
harshvegada <harshhpatel07@gmail.com>
2023-11-21 08:57:07 +0530

    Adding packages

---
+++ src/samitasansare/LaunchChromeBrowser.java
@@ -0,0 +1,14 @@
+package samitasansare;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChromeBrowser {
+
+	
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", ".\\chromeDriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();//this will launch chrome browser
+		driver.get("https://www.amazon.in/");//this will navigate to url
+	}
+}

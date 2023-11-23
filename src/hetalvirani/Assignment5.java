8a115ce0bcb3353f7c7311034911dfb8713ffe3f
Hetalvirani06 <hetalusadadiya@gmail.com>
2023-11-23 17:19:23 +0530

    Hetal_Assignment5

---
+++ src/hetalvirani/Assignment5.java
@@ -0,0 +1,61 @@
+package hetalvirani;
+
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Assignment5 {
+
+	public static void main(String[] args) {
+		System.out.println("Step - Launch chrome browser");
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\shree\\eclipse-workspace\\SEP23Selenium\\chromedriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+		System.out.println("Step - Maximize browser");
+		driver.manage().window().maximize();
+
+		System.out.println("step - Launch automationbykrishna.com");
+		driver.navigate().to("http://automationbykrishna.com/");
+
+		System.out.println("step - click on Basicelements");
+		WebElement basicElement = driver.findElement(By.id("basicelements"));
+		basicElement.click();
+
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		WebElement javascriptBtn = driver.findElement(By.xpath("//button[@id='javascriptConfirmBox']"));
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].scrollIntoView(true)", javascriptBtn);
+		javascriptBtn.click();
+
+		System.out.println("VERIFY - Alert message should be as expected");
+		Alert alert = driver.switchTo().alert();
+		String alertText = alert.getText();
+		System.out.println("AlertText is " + alertText);
+		if (alertText.endsWith(alertText)) {
+			System.out.println("Test pass");
+		} else {
+			System.out.println("Test fail");
+		}
+
+		alert.dismiss();
+		String text = driver.findElement(By.xpath("//p[@id='pgraphdemo']")).getText();
+		if (text.equals("ok")) {
+			System.out.println(text);
+		} else {
+			System.out.println(text);
+		}
+		 driver.close();
+	}
+}

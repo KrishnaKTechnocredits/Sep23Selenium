c74912443d6fa2d905d7cf0c2548cc822eb1295e
Jaykumar Bankar <bankarjaykumar@gmail.com>
2023-11-24 23:00:28 +0530

    Jaykumar_Assignment08

---
+++ src/jaykumar/Assignment8.java
@@ -0,0 +1,92 @@
+/*Assignment8 Nov19 (Dropdown)
+- Navigate to https://www.facebook.com/
+- Click on 'Create new accout'
+- Enter name, surname, mobile or email & password
+- Select DOB
+- Select Gender (Actual gender)
+Hint: Use select class
+selectByText("");
+selectByValue("");
+selectByIndex();*/
+
+package jaykumar;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Assignment8 {
+	WebDriver driver;
+	
+	void setUp(String url){
+		System.out.println("STEP - Launch chrome browser");
+		System.setProperty("webdriver.chrome.driver", ".\\chromeDriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		System.out.println("STEP - Launch: "+url);
+		driver.get(url);
+	}
+	
+	void waitABit(int ms) {
+		try {
+			Thread.sleep(ms);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	void submitDetails(String fName, String lName, String mob, String password) {
+		System.out.println("STEP - Enter Details");
+		driver.findElement(By.xpath("//input[@name='firstname']")).sendKeys(fName);
+		driver.findElement(By.xpath("//input[@name='lastname']")).sendKeys(lName);
+		driver.findElement(By.xpath("//input[@name='reg_email__']")).sendKeys(mob);
+		driver.findElement(By.xpath("//input[@name='reg_passwd__']")).sendKeys(password);
+	}
+	
+	Select selectDropDown(WebElement e) {
+		Select oSelect = new Select(e);
+		return oSelect;
+	}
+	
+	void selectTestCases() {
+		setUp("https://www.facebook.com/");
+		
+		System.out.println("STEP - Click on Create New Account");
+		driver.findElement(By.xpath("//a[text()='Create new account']")).click();
+		waitABit(3000);
+		
+		submitDetails("Jaykumar", "Bankar", "123456", "Jay123456");
+		
+		System.out.println("STEP - Enter DOB");
+		WebElement dayElement = driver.findElement(By.xpath("//select[@id='day']"));
+		selectDropDown(dayElement).selectByVisibleText("13");
+		
+		WebElement monthElement = driver.findElement(By.xpath("//select[@id='month']"));
+		selectDropDown(monthElement).selectByIndex(10);
+		
+		WebElement yearElement = driver.findElement(By.xpath("//select[@id='year']"));
+		selectDropDown(yearElement).selectByValue("1997");
+		
+		
+		System.out.println("STEP - Select gender");
+		WebElement genderElement = driver.findElement(By.xpath("//input[@value='2']"));
+		genderElement.click();
+		
+		System.out.println("STEP - Verify gender selection");
+		if(genderElement.isSelected()) {
+			System.out.println("Gender selection test passed");
+		}else {
+			System.out.println("Gender selection test failed");
+		}
+		
+		System.out.println("STEP - Close browser");
+		driver.close();
+	}
+	
+	public static void main(String[] args) {
+		new Assignment8().selectTestCases();
+	}
+}

f49a9470c0bbce72acf187d1b43ca5b154dc8fd4
patilonkar18 <patilonkar18@gmail.com>
2023-11-25 20:28:53 +0530

    Onkar_Assignment13

---
+++ src/onkar/Assignment13.java
@@ -0,0 +1,64 @@
+/*Print Last row data, Demo tables >> EMPLOYEE BASIC INFORMATION table
+Note: Get row size - 4*/
+
+package onkar;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assignment13 {
+
+	WebDriver driver;
+
+	void setup(String url) {
+		System.out.println("Launch chrome browser");
+		System.setProperty("webdriver.chrome.driver", ".\\chromeDriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+
+		System.out.println("Maximize browser window");
+		driver.manage().window().maximize();
+
+		System.out.println("Open given URL");
+		driver.get(url);
+	}
+
+	void sleep(int ms) {
+		try {
+			Thread.sleep(ms);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	void findNumberOfRowsAndColumns() {
+		setup("http://automationbykrishna.com");
+
+		System.out.println("Navigate to Demo Tables");
+		driver.findElement(By.xpath("//a[@id='demotable']")).click();
+		sleep(3000);
+
+		List<WebElement> rows = driver.findElements(By.xpath("//table[@id='table1']/tbody/tr"));
+		int rowCount = rows.size();
+		
+		List<WebElement> columns =
+		driver.findElements(By.xpath("//table[@id='table1']/thead/tr/th")); 
+		int columnCount = columns.size();
+		
+		System.out.print("Last row of the given table is : ");
+		for(int index=1; index<=columnCount; index++) {
+			System.out.print(driver.findElement(By.xpath("//table[@id='table1']/tbody/tr["+rowCount+"]/td["+index+"]")).getText());
+			System.out.print(" ");
+		}
+		
+		System.out.println("\n"+"Closed the browser");
+		driver.close();
+	}
+
+	public static void main(String[] args) {
+		new Assignment13().findNumberOfRowsAndColumns();
+	}
+}

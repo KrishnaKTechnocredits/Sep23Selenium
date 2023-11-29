ea058dc9dd41ad12d7e7df0ae7ed17ab1750caf2
Denish Santoki <denishsantoki74@.com>
2023-11-29 18:07:22 +0530

    Denish_Assignment_20

---
+++ src/denishsantoki/Assignment20.java
@@ -0,0 +1,58 @@
+/*Assignment20: 25 Nov
+Verify number of employees working at each position.
+1. Navigate to https://datatables.net/
+2. Find employees working at each position.
+3. Verify number of employees.
+*/
+package denishsantoki;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Assignment20 {
+	
+	WebDriver driver;
+
+	@BeforeClass
+	void setup() {
+		System.out.println("Launch chrome browser");
+		System.setProperty("webdriver.chrome.driver", ".\\chromeDriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+
+		System.out.println("Maximize browser window");
+		driver.manage().window().maximize();
+
+		System.out.println("Open given URL");
+		driver.get("https://datatables.net/");
+	}
+
+	@Test
+	void empPositions() {
+		List<WebElement> listOfPosition = driver.findElements(By.xpath("//table[@id='example']/tbody/tr/td[2]"));
+		Map<String, Integer> deptMap = new LinkedHashMap<String, Integer>();
+		for (WebElement name : listOfPosition) {
+			String dept = name.getText();
+			if (deptMap.containsKey(dept)) {
+				int count = deptMap.get(dept);
+				deptMap.put(dept, count + 1);
+			} else {
+				deptMap.put(dept, 1);
+			}
+		}
+		System.out.println("Number Of Employees Working At Each Position : " + deptMap);
+	}
+
+	@AfterClass
+	void tearDown() {
+		driver.close();
+	}
+}

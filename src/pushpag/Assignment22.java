22aded3567112f1b350a649ae2c287135c5fc0fd
pushpagite <pushpagite21@gmail.com>
2023-12-01 18:41:51 +0530

    Pushpa Assignment22

---
+++ src/pushpag/Assignment22.java
@@ -0,0 +1,78 @@
+//
+//Assignment22: 25 Nov
+//Verify list of employees whose age is less than 50 and works in London office.
+//1. Navigate to https://datatables.net/
+//2. Find employees whose age is less than 50 and works in London office.
+//3. Verify list of employees.
+//Note: 
+//Create expected list and compare with actual list.
+
+package pushpag;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class Assignment22 {
+	WebDriver driver;
+
+	@BeforeMethod
+	public void setBrowser() {
+		System.setProperty("webdriver.chrome.driver", ".\\chromeDriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+		// Navigate to https://datatables.net/
+		System.out.println("Step 1 - https://datatables.net/");
+		driver.navigate().to("https://datatables.net/");
+		wait(3000);
+	}
+
+	@org.testng.annotations.Test
+	public void findListOfEmployees() throws InterruptedException {
+		List<WebElement> tableRows = driver.findElements(By.xpath("//table[@id = 'example']//tbody//tr"));
+		wait(3000);
+		String list[] = { "Angelica Ramos", "Bradley Greer", "Bruno Nash" };
+		List<String> expectedList = Arrays.asList(list);
+		ArrayList<String> listOfEmp = new ArrayList<String>();
+		for (int index = 1; index <= tableRows.size(); index++) {
+			String empName = driver.findElement(By.xpath("//table[@id = 'example']//tbody//tr[" + index + "]//td[1]"))
+					.getText();
+			String office = driver.findElement(By.xpath("//table[@id = 'example']//tbody//tr[" + index + "]//td[3]"))
+					.getText();
+			int age = Integer.parseInt(driver
+					.findElement(By.xpath("//table[@id = 'example']//tbody//tr[" + index + "]//td[4]")).getText());
+
+			if (age < 50 && office.equals("London"))
+				listOfEmp.add(empName);
+		}
+		System.out.println("List of Employees whose age < 50  and works in London office : " + listOfEmp);
+
+		if (expectedList.equals(listOfEmp))
+			System.out.println("Test Case Passed...");
+		else
+			System.out.println("Test Case Failed...");
+	}
+
+	public void wait(int ms) {
+		try {
+			Thread.sleep(ms);
+		} catch (InterruptedException ie) {
+			ie.printStackTrace();
+		}
+	}
+
+	@AfterMethod
+	public void closeDriver() {
+		driver.close();
+	}
+}
